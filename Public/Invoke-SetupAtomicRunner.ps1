65855b0c7e8c5e7d0603596ba40fb6d238ee74c4
Hare Sudhan <code@0x6c.dev>
2024-09-05 15:22:36 -0400

    running tests

--- Public/Invoke-SetupAtomicRunner.ps1
+++ Public/Invoke-SetupAtomicRunner.ps1
@@ -32,7 +32,7 @@ function Invoke-SetupAtomicRunner {
         }
     }
 
-    if ($artConfig.basehostname.length -gt 15) { Throw "The hostname for this machine (minus the GUID) must be 15 characters or less. Please rename this computer." }
+    # if ($artConfig.basehostname.length -gt 15) { Throw "The hostname for this machine (minus the GUID) must be 15 characters or less. Please rename this computer." }
 
     #create AtomicRunner-Logs directories if they don't exist
     New-Item -ItemType Directory $artConfig.atomicLogsPath -ErrorAction Ignore
